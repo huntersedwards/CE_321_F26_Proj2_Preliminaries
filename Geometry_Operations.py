880b1388a36ec9a59c55702cb1c4bacd1eedba56
huntersedwards <may62003@gmail.com>
2026-09-25 00:35:45 -0600

    Update Geometry_Operations.py

--- Geometry_Operations.py
+++ Geometry_Operations.py
@@ -12,48 +12,89 @@ import sys
 
 # length of the beam
 def Length(bar):
-    return
+    vector = BarNodeToVector(bar.init_node, bar)
+    length = VectorTwoNorm(vector)
+    return length
 
 # Find two norm (magnitude) of a vector
 def VectorTwoNorm(vector):
-    return
+    value = 0
+    for val in vector:
+        value = value + val**2
+    return math.sqrt(value)
 
 # Find a shared node between two bars
 def FindSharedNode(bar_1,bar_2):
-    return
+    if bar_1.init_node == bar_2.init_node:
+        return bar_1.init_node
+    elif bar_1.init_node == bar_2.end_node:
+       return bar_1.init_node
+    elif bar_1.end_node == bar_2.init_node:
+       return bar_1.end_node
+    elif bar_1.end_node == bar_2.end_node:
+        return bar_1.end_node
+    else:
+        sys.exit("These two bars do not share a node.")
 
 # Given a bar and a node on that bar, find the other node
 def FindOtherNode(node,bar):
+    if (bar.end_node == node):
+        return bar.init_node
+    elif (bar.init_node == node):
+        return bar.end_node
+    else:
+        sys.exit("The node that you input is not on the input bar.")
     return
 
 # Find a vector from input node (of the input bar) in the direction of the bar
 def BarNodeToVector(origin_node,bar):
-    return
+    [origin_x, origin_y] = origin_node.location
+    other_node = FindOtherNode(origin_node, bar)
+    [other_x, other_y] = other_node.location   
+    vec = [other_x - origin_x, other_y - origin_y]
+    return vec
 
 # Convert to bars that meet at a node into vectors pointing away from that node
 def BarsToVectors(bar_1,bar_2):
-    return
+    Node = FindSharedNode(bar_1, bar_2)
+    vec1 = BarNodeToVector(bar_1, Node)
+    vec2 = BarNodeToVector(bar_2, Node)
+    return vec1, vec2
 
 # Cross product of two vectors
 def TwoDCrossProduct(vec1,vec2):
-    return
+    Cross_Product = vec1(0)*vec2(1)-vec1(1)*vec2(2)
+    return Cross_Product
 
 # Dot product of two vectors
 def DotProduct(vec1,vec2):
-    return
+    Dot_Product = vec1(0)*vec2(0) + vec1(1)*vec2(1)
+    return Dot_Product
 
 # Cosine of angle from local x vector direction to other vector
 def CosineVectors(local_x_vec,other_vec):
-    return
+    Dot_Product = DotProduct(local_x_vec, other_vec)
+    Norm_vec_1 = VectorTwoNorm(local_x_vec)
+    Norm_vec_2 = VectorTwoNorm(other_vec)
+    Cosine = Dot_Product / (Norm_vec_1 * Norm_vec_2)
+    return Cosine
 
 # Sine of angle from local x vector direction to other vector
 def SineVectors(local_x_vec,other_vec):
-    return
+    Cross_Product = TwoDCrossProduct(local_x_vec, other_vec)
+    Norm_vec_1 = VectorTwoNorm(local_x_vec)
+    Norm_vec_2 = VectorTwoNorm(other_vec)
+    Sine = Cross_Product / (Norm_vec_1 * Norm_vec_2)
+    return Sine
 
 # Cosine of angle from local x bar to the other bar
 def CosineBars(local_x_bar,other_bar):
-    return
+    vec1, vec2 = BarsToVectors(local_x_bar, other_bar)
+    Cosine = CosineVectors(vec1, vec2)
+    return Cosine
 
 # Sine of angle from local x bar to the other bar
 def SineBars(local_x_bar,other_bar):
-    return
+    vec1, vec2 = BarsToVectors(local_x_bar, other_bar)
+    Sine = SineVectors(vec1, vec2)
+    return Sine
